b6e85f32a5c5a7c870c9be7009907653390e7143
Julian Montez <julian.montez@gmail.com>
2014-07-14 15:24:17 -0500

    Improve clarity of building the test command

--- taas/frameworks.py
+++ taas/frameworks.py
@@ -61,26 +61,22 @@ class Tempest(Framework):
             checkout = 'git clone {0} {1}'.format(repo, tempest_dir)
             subprocess.check_call(checkout, shell=True)
 
-        json_file = 'taas_results.json'
-        json_flag = '--with-json --json-file={0}'.format(json_file)
+        tests_file = abspath('results.json')
+        tests_dir = join(tempest_dir, 'tempest/api/%s' % self.test)
 
-        tempest_cmd = (
-            'python -u `which nosetests` --where='
-            '{0}/tempest/api/{1} {2}'.format(tempest_dir, self.test,
-                                             json_flag)
-        )
+        self.populate_settings()
 
-        LOG.debug('Tempest command: {0}'.format(tempest_cmd))
+        flags = '--with-json --json-file={0}'.format(tests_file)
+        tempest_cmd = 'nosetests --where={0} {1}'.format(tests_dir, flags)
+
+        LOG.info('Running Tempest tests for: {0}'.format(self.test))
 
         try:
-            subprocess.check_output(
-                tempest_cmd,
-                shell=True,
-                stderr=subprocess.STDOUT
-            )
+            subprocess.check_output(tempest_cmd, shell=True,
+                                    stderr=subprocess.STDOUT)
         except Exception as exc:
             LOG.error(exc.output)
 
-        with open(json_file, 'r') as fp:
+        with open(tests_file, 'r') as fp:
             return json.dumps(json.load(fp), sort_keys=True, indent=4,
                               separators=(',', ': '))
